4400bef8a9c600c9708db3839ac4179f4807b0d8
caics <caics.china@gmail.com>
2025-01-06 22:38:54 +1300

    address autocomplete bug

---
+++ BlazorApp/wwwroot/address.js
@@ -0,0 +1,8 @@
+﻿export function initializeAddressAutocomplete(elementId, dotNetHelper) {
+    const input = document.getElementById(elementId);
+    input.addEventListener('input', async function () {
+        const query = input.value;
+        const results = ["a", "b", "c"];
+        dotNetHelper.invokeMethodAsync('PopupAddresses', results);
+    });
+}
